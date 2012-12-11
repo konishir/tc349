257976d9fb4522c4d9dce21471775fd545e25f1b
Rebecca Konishi <konishir@msu.edu>
2012-12-10 20:05:38 -0500

    final

---
+++ projects/final/js/origami.js
@@ -0,0 +1,9 @@
+function upSize(i){
+document.getElementById (i).style.height = '150px';
+document.getElementById (i).style.width = '150px';
+}
+
+function downSize(i){
+document.getElementById (i).style.height = '100px';
+document.getElementById (i).style.width = '100px';
+}
